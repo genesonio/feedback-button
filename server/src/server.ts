5016b5f8022a9ba5fb9a68970a91062b10fd97d6
Genesio S Pacehco <genesio.s.p.28@gmail.com>
2022-05-14 14:01:30 -0300

    Adding jest to unitary test

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes'
 
 const app = express()
@@ -10,6 +11,10 @@ const app = express()
 // PATCH = Atualizar uma informação única de uma entidade
 // DELETE = Deletar uma informação
 
+
+app.use(cors({
+  origin: 'http://localhost:3333'
+}))
 app.use(express.json())
 app.use(routes)
 
